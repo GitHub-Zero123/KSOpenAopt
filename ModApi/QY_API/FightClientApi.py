5eb5079569203717f2bf31c5d0fe59056def4fc8
Zero123 <11438691+bili_zero123@user.noreply.gitee.com>
2025-01-07 13:09:22 +0800

    剑魂库更新

--- ModApi/QY_API/FightClientApi.py
+++ ModApi/QY_API/FightClientApi.py
@@ -4,6 +4,8 @@ import mod.client.extraClientApi as clientApi
 def RegisterKnifeLight(knifeLightList):
     Config = clientApi.ImportModule("SwordSoulScripts.Config")
     for knifeLight in knifeLightList:
+        if "textures/models/" in knifeLight:
+            knifeLight.strip("textures/models/")
         Config.KnifeLightTextureData.append([knifeLight])
 
 def RegisterSkin(skin_id, name=None, icon="", texture=""):
@@ -33,6 +35,28 @@ def RegisterModel(skin_id, name=None, icon="", texture="", geometry="", default_
         "default_animation": default_animation
     }
     Config.ModelData.append(Data)
+
+class FightDataSetting:
+    @staticmethod
+    def RegisterAttackCommon(Data):
+        return clientApi.ImportModule("SwordSoulScripts.SwordSoul").RegisterAttackCommon(Data)
+
+class FightSystemSetting:
+    @staticmethod
+    def GetBetterAnimationToggle():
+        return clientApi.ImportModule("SwordSoulScripts.SwordSoul").ConfigData["Better"]
+
+    @staticmethod
+    def GetSuperFightToggle():
+        return clientApi.ImportModule("SwordSoulScripts.SwordSoul").ConfigData["SuperFight"]
+
+    @staticmethod
+    def SetBetterAnimationToggle(State):
+        return clientApi.ImportModule("SwordSoulScripts.SwordSoul").SetBetterAnimationToggle(State)
+
+    @staticmethod
+    def SetSuperFightToggle(State):
+        return clientApi.ImportModule("SwordSoulScripts.SwordSoul").SetSuperFightToggle(State)
 
 def HasSwordSoul():
     return clientApi.ImportModule("SwordSoulScripts.__init__")
